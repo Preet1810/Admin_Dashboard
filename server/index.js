61614cdd0159955e4eb9fc4221f9b111ca7dae8e
Preet1810 <preetpalsingh1239@gmail.com>
2023-02-05 17:34:03 +0530

    userdata

--- server/index.js
+++ server/index.js
@@ -10,6 +10,22 @@ import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
+// data imports
+import User from "./models/User.js";
+// import Product from "./models/Product.js";
+// import ProductStat from "./models/ProductStat.js";
+// import Transaction from "./models/Transaction.js";
+// import OverallStat from "./models/OverallStat.js";
+// import AffiliateStat from "./models/AffiliateStat.js";
+import {
+    dataUser
+    // dataProduct,
+    // dataProductStat,
+    // dataTransaction,
+    // dataOverallStat,
+    // dataAffiliateStat,
+} from "./data/index.js";
+
 /* CONFIGURATION */
 dotenv.config();
 const app=express();
@@ -36,5 +52,6 @@ mongoose.connect(process.env.MONGO_URL, {
 })
     .then(() => {
         app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+        // User.insertMany(dataUser);
     })
     .catch((error) => console.log(`${error} did not connect`));
