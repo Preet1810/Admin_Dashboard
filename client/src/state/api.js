9ef234b130aa01d9171c83a5c82b375a508fa627
Preet1810 <preetpalsingh1239@gmail.com>
2023-02-08 09:53:15 +0530

    stats

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -4,7 +4,7 @@ export const api=createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
     reducerPath: "adminApi",
     tagTypes: [
-        "User", "Products", "Customers", "Transactions"
+        "User", "Products", "Customers", "Transactions", "Geography", "Sales"
     ],
     endpoints: (build) => ({
         getUser: build.query({
@@ -26,10 +26,18 @@ export const api=createApi({
                 params: { page, pageSize, sort, search },
             }),
             providesTags: ["Transactions"]
+        }),
+        getGeography: build.query({
+            query: () => "client/geography",
+            providesTags: ["Geography"]
+        }),
+        getSales: build.query({
+            query: () => "sales/sales",
+            providesTags: ["Sales"]
         })
     })
 })
 
 export const {
-    useGetUserQuery, useGetProductsQuery, useGetCustomersQuery, useGetTransactionsQuery
+    useGetUserQuery, useGetProductsQuery, useGetCustomersQuery, useGetTransactionsQuery, useGetGeographyQuery, useGetSalesQuery
 }=api;
--- server/index.js
+++ server/index.js
@@ -15,14 +15,14 @@ import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
-// import OverallStat from "./models/OverallStat.js";
+import OverallStat from "./models/OverallStat.js";
 // import AffiliateStat from "./models/AffiliateStat.js";
 import {
     dataUser,
     dataProduct,
     dataProductStat,
     dataTransaction,
-    // dataOverallStat,
+    dataOverallStat,
     // dataAffiliateStat,
 } from "./data/index.js";
 
@@ -56,5 +56,6 @@ mongoose.connect(process.env.MONGO_URL, {
         // Product.insertMany(dataProduct);
         // ProductStat.insertMany(dataProductStat);
         // Transaction.insertMany(dataTransaction);
+        // OverallStat.insertMany(dataOverallStat);
     })
     .catch((error) => console.log(`${error} did not connect`));
